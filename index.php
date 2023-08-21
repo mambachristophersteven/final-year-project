732c2bddcfebda94e234b81b2bdac3b4a469180a
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-20 20:25:19 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="52x52" href="./assets/images/meta-logo-black.png">
+    <link rel="stylesheet" href="./styles/global.css">
+    <link rel="stylesheet" href="./styles/ind.css">
+    <title>Winkies</title>
+</head>
+<body>
+    <div class="container">
+        <div class="logo-and-tagline">
+            <div class="logo">
+                <img src="./assets/images/logo-light.png" alt="">
+            </div>
+            <div class="tagline">
+                <p class="brag">The best in town</p>
+                <p class="small-brag">Tasty meals just for you. <br>Let your tastebuds <br>explode</p>
+            </div>
+        </div>    
+        <div class="button-div">
+            <a href="./selectLogin.php" class="button">
+                <button>Get Started</button>
+            </a>
+        </div>
+        <div class="power">
+            <img src="./assets/images/mamba.png" alt="">
+            <p class="developer">powered by mambalev</p>
+        </div>
+    </div>
+    
+</body>
+</html>
---
+++ selectLogin.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="52x52" href="./assets/images/meta-logo-black.png">
+    <link rel="stylesheet" href="./styles/global.css">
+    <link rel="stylesheet" href="./styles/selectLogi.css">
+    <title>Winkies- Select Login</title>
+</head>
+<body>
+    <div class="container">
+        <div class="top">
+            <img src="./assets/images/logo-light.png" alt="logo">
+            <a href="./index.php">
+                <img src="./assets//icons/back.svg" alt="">
+            </a>
+        </div>
+        <div class="bottom">
+            <div class="first">
+                <p>Welcome to winkies</p>
+                <p>Get straight to ordering your food</p>
+                <div class="button-div">
+                    <a href="#"><button class="button">Order Now</button></a>
+                </div>
+            </div>
+            <div class="second">
+                <p>Have an account with us?</p>
+                <div class="button-div">
+                    <a href="#"><button class="button">Login</button></a>
+                </div>
+            </div>
+            <div class="third">
+                <p>Want to join our account holders?</p>
+                <div class="button-div">
+                    <a href="#"><button class="button">Sign Up</button></a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
+</body>
+</html>
