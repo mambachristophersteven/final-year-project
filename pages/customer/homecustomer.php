78f2b7ea782c0f869db63daf49b9a9ade696de0d
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-26 23:22:51 -0700

    content of customer home page added

--- pages/customer/homecustomer.php
+++ pages/customer/homecustomer.php
@@ -35,6 +35,79 @@ $position=$row['role'];
             <p class="name">Hello, <?php echo $username; ?>.</p>
             <p class="motivation">Get down to ordering something, anything.</p>
         </div>
+        <div class="search">
+            <form action="#">
+                <div class="input">
+                    <input type="text" name="search" id="search" placeholder="search all meals">
+                    <img src="../../assets/icons/search.svg" alt="">
+                </div>
+                <input type="submit" value="search" name="search" id="searchButton">
+            </form>
+        </div>
+        <div class="best-selling-section">
+            <div class="top-section">
+                <p class="section-title">best selling today</p>
+                <p class="section-small">This is what people are buying the most today</p>
+            </div>
+            <div class="meal-box">
+                <a href="#">
+                    <img src="../../assets/meals/9.svg" alt="meal-image" class="meal-image">
+                </a>
+                <p class="meal-name">The plate chritopher</p>
+                <p class="meal-price">¢45.00</p>
+                <p class="meal-description">Originating from the tribe of Ubuntu, this meal is the taste of....</p>
+                <div class="button-and-heart">
+                    <a href="#">
+                        <button>order now</button>
+                    </a>
+                    <button class="like-button">
+                        <img src="../../assets/icons/like.svg" alt="like button" id="like">
+                    </button>
+                    <button class="like-button">
+                        <img src="../../assets/icons/liked.svg" alt="like button" id="liked">
+                    </button>
+                </div>
+            </div>
+        </div>
+        <div class="recommended">
+            <div class="top-section">
+            <p class="section-title">Recommended</p>
+            <p class="section-small">We recommend these meals for you. Try them</p>
+            <div class="meals">
+                <div class="recommended-meal">
+                    <div class="button-only">
+                        <button>
+                            <img src="../../assets/icons//like.svg" alt="like-button">
+                        </button>
+                    </div>
+                    <a href="#">
+                        <img src="../../assets/meals/11.svg" alt="meal-image">
+                    </a>  
+                    <div class="meal-info">
+                        <p class="rmeal-name">Black Panther</p>
+                        <p class="rmeal-price">¢18.00</p>
+                        <p class="rmeal-description">Originating from the tribe of Ubuntu, this meal is the taste of....</p>
+                    </div>               
+                </div>
+                <div class="recommended-meal">
+                    <div class="button-only">
+                        <button>
+                            <img src="../../assets/icons//like.svg" alt="like-button">
+                        </button>
+                    </div>
+                    <a href="#">
+                        <img src="../../assets/meals/11.svg" alt="meal-image">
+                    </a>  
+                    <div class="meal-info">
+                        <p class="rmeal-name">Black Panther</p>
+                        <p class="rmeal-price">¢18.00</p>
+                        <p class="rmeal-description">Originating from the tribe of Ubuntu, this meal is the taste of....</p>
+                    </div>               
+                </div>
+            </div>
+            </div>
+        </div>
+
     </div>
     
 </body>
