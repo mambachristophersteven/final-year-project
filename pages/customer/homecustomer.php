74f292078957e4e3ac0431dcb52c2a64cc3cf790
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-26 17:39:17 -0700

    starting customer home page content

--- pages/customer/homecustomer.php
+++ pages/customer/homecustomer.php
@@ -13,7 +13,6 @@ $nums= mysqli_num_rows($result);
 $row= mysqli_fetch_assoc($result);
 $position=$row['role'];
 
-
 ?>
 
 <!DOCTYPE html>
@@ -23,11 +22,20 @@ $position=$row['role'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="52x52" href="../../assets/images/meta-logo-black.png">
     <link rel="stylesheet" href="../../styles/global.css">
-    <link rel="stylesheet" href="../..//styles/chefho.css">
-    <title>Winkies - Home</title>
+    <link rel="stylesheet" href="../..//styles/customerhome.css">
+    <title>Winkies - Customer</title>
 </head>
 <body>
-    <h1>heyyyyy</h1>
+    <div class="container">
+    <div class="top">
+            <img src="../../assets/images/logo-light.svg" alt="logo">
+            <img src="../../assets/avatars/9.svg" alt="">
+        </div>
+        <div class="welcome">
+            <p class="name">Hello, <?php echo $username; ?>.</p>
+            <p class="motivation">Get down to ordering something, anything.</p>
+        </div>
+    </div>
     
 </body>
 </html>
