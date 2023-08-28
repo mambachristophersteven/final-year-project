446a3e4f063314f78c71c56d79cb1ed296c30935
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-28 15:43:59 -0700

    starting interactivity for meal page

---
+++ interactivity/individualmea.js
@@ -0,0 +1,14 @@
+const likeButton = document.getElementById('like');
+const likedButton = document.getElementById('liked');
+
+
+likeButton.addEventListener('click', ()=>{
+    likeButton.style.display = 'none'
+    likedButton.style.display = 'flex'
+})
+likedButton.addEventListener('click', ()=>{
+    likedButton.style.display = 'none'
+    likeButton.style.display = 'flex'
+})
+
+const 
