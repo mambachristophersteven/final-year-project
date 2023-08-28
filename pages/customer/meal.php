0b7e302d064ee0a6f152c7890b8d67e0454532ad
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-28 09:18:58 -0700

    starting individual meal page

---
+++ pages/customer/meal.php
@@ -0,0 +1,38 @@
+<?php
+
+include '../../connection.php'; 
+session_start();
+if(!isset($_SESSION['username'])){
+    header("location: ../index.php");
+}
+
+$username= $_SESSION['username'];
+$sql= "SELECT * FROM `users` WHERE username= '$username'";
+$result= mysqli_query($con,$sql);
+$nums= mysqli_num_rows($result);
+$row= mysqli_fetch_assoc($result);
+$email=$row['email'];
+$date_joined=$row['date_joined'];
+
+$sqlmeals = "SELECT * FROM `meals`";
+$resultmeals = mysqli_query($con,$sqlmeals);
+$numsmeals = mysqli_num_rows($resultmeals);
+$rowmeals = mysqli_fetch_assoc($resultmeals);
+
+$sqlmeals_on_menu = "SELECT * FROM `meals` WHERE on_menu = 'true'";
+$resultmeals_on_menu = mysqli_query($con,$sqlmeals_on_menu);
+$numsmeals_on_menu = mysqli_num_rows($resultmeals_on_menu);
+$rowmeals_on_menu = mysqli_fetch_assoc($resultmeals_on_menu);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
