de9569fe190d8c63da1920079991f06bcdef5525
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-31 06:51:23 -0700

    added styles for chef orders page

--- pages/chef/orderschef.php
+++ pages/chef/orderschef.php
@@ -50,7 +50,7 @@ $numOfOrders= mysqli_num_rows($resultorder)-1;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" sizes="52x52" href="../../assets/images/meta-logo-black.png">
     <link rel="stylesheet" href="../../styles/global.css">
-    <link rel="stylesheet" href="../../styles/ordersche.css">
+    <link rel="stylesheet" href="../../styles/ordersc.css">
     <title>Winkies - Chef</title>
 </head>
 <body>
