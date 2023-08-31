34eec607af3f5070a23b387460cac108fb2acb26
Christopher Steven <christopherstevennunoo@gmail.com>
2023-08-31 10:17:26 -0700

    added interactivity for admin home page

--- pages/admin/homeadmin.php
+++ pages/admin/homeadmin.php
@@ -12,6 +12,8 @@ $result= mysqli_query($con,$sql);
 $nums= mysqli_num_rows($result);
 $row= mysqli_fetch_assoc($result);
 $position=$row['role'];
+$email=$row['email'];
+$date_joined=$row['date_joined'];
 
 $sqlmeals = "SELECT * FROM `meals`";
 $resultmeals = mysqli_query($con,$sqlmeals);
@@ -49,11 +51,40 @@ $row= mysqli_fetch_assoc($resultcustomer);
 <div class="container">
         <div class="top">
             <img src="../../assets/images/logo-light.svg" alt="logo">
-            <img src="../../assets/avatars/9.svg" alt="">
+            <img src="../../assets/avatars/9.svg" alt="" id="user-profile">
         </div>
         <div class="welcome">
             <p class="name">Hello, Admin <?php echo $username; ?>.</p>
             <p class="motivation">Check and know what ever is happening.</p>
+        </div>
+        <div class="user-profile" id="user-profile-box">
+            <img src="../../assets/icons/close.svg" alt="" id="close">
+            <div class="user-avatar">
+                <p class="user">User Profile</p>
+                <img src="../../assets/avatars/9.svg" alt="user avatar">
+            </div>
+            <div class="user-info">
+                <div class="info">
+                    <p class="info-title">username</p>
+                    <p class="info-value"><?php echo $username;?></p>
+                </div>
+                <div class="info">
+                    <p class="info-title">email</p>
+                    <p class="info-value"><?php echo $email;?></p>
+                </div>
+                <div class="info">
+                    <p class="info-title">date joined</p>
+                    <p class="info-value"><?php echo $date_joined;?></p>
+                </div>
+            </div>
+            <div class="user-buttons">
+                <a href="#">
+                    <button id="edit">edit profile info</button>
+                </a>
+                <a href="../../logout.php">
+                    <button id="logout">logout</button>
+                </a>
+            </div>
         </div>
         <p class="page">Overview</p>
         <div class="boxes">
@@ -166,6 +197,6 @@ $row= mysqli_fetch_assoc($resultcustomer);
         </div>
     </div>
 
-    
+    <script src="../../interactivity/homeadmin.js"></script>
 </body>
 </html>
